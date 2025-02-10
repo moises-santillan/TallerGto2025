c91ab570f24720fa7eb624a7fbe4db4ca917a71c
Moises Santillan <moises.santillan.zeron@gmail.com>
2025-02-10 10:26:54 -0600

    $

--- Code/gato.py
+++ Code/gato.py
@@ -1,8 +1,9 @@
 import library as lb
 import numpy as np
 
+g = np.loadtxt("glucose_control.txt")
 t_data=[0,20,40,60,80,100,120]
-glucose_data=[1,1.526,1.777,1.47,1.447,1.447,1.149]
+glucose_data=g[2, :]
 
 
 lb.fit_OGTT_ga(t_data, glucose_data)
