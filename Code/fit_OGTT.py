5f82211dfe98e9291083d21bebc90e8df4218256
Moises Santillan <moises.santillan.zeron@gmail.com>
2025-02-10 23:56:02 -0600

    $

--- Code/fit_OGTT.py
+++ Code/fit_OGTT.py
@@ -1,9 +1,10 @@
 import library as lb
 import numpy as np
 
-g = np.loadtxt("glucose_control.txt", delimiter=",")
+i=4
+g = np.loadtxt("glucose_control.txt")
 t_data=[0,20,40,60,80,100,120]
-glucose_data=g[4, :]
+glucose_data=g[i, :]
 
 
 lb.fit_OGTT_ga(t_data, glucose_data)
